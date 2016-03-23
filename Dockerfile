c3b156ec4fc38e70b4fb73ef2e1fc24dd5f6187f
Sebastian <sep.gil@gmail.com>
2016-03-23 22:48:05 +0100

    Added configuration via variables.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
-FROM mhart/alpine-node:4.4
+FROM node:4.4
 
 WORKDIR /src
 ADD . .
-
-# If you have native dependencies, you'll need extra tools
-RUN apk add --no-cache make gcc g++ python git
 
 # If you need npm, don't use a base tag
 RUN npm install
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 var Particle = require('particle-api-js');
 var graphite = require('graphite');
+var nconf = require('nconf');
+
+nconf.argv()
+  .env();
+
+
+if (!nconf.get('device') || !nconf.get('token')) {
+  console.error('Device or token not set!');
+  process.exit(1);
+}
 
 var particle = new Particle();
 
-var particleToken = '2f556a89ad71889f2195f85bc787db46aa5f6804';
-var particleDevice = '55ff66065075555342421787';
+var particleToken = nconf.get('token');
+var particleDevice = nconf.get('device');
 
-var client = graphite.createClient('plaintext://guest:guest@192.168.99.100:2003/');
+console.log(`Creating graphite client on ${nconf.get('graphite')}`);
+var client = graphite.createClient(nconf.get('graphite'));
 
-console.log("Started logging");
+console.log(client);
 
 particle.getEventStream({deviceId: particleDevice, auth: particleToken, name: '/weather'}).then((stream) => {
   stream.on('event', function (data) {
     var time = new Date(data.published_at);
     try {
+      console.log(data.data);
       var weather = JSON.parse(data.data);
 
       var temp = {
@@ -42,4 +54,7 @@ particle.getEventStream({deviceId: particleDevice, auth: particleToken, name: '/
       console.error(`Could not parse weather data from particle: ${e}`);
     }
   });
+}).catch((error) => {
+  console.err(`Could not connect to particle cloud: ${error}`);
+  process.exit(1);
 });
